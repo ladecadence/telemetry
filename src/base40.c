57a793ee890103058d5e1128472bdb52c1d8a357
David Pello <zako@ladecadence.net>
2022-01-04 17:44:41 +0100

    cleanup tabs

--- src/base40.c
+++ src/base40.c
@@ -2,46 +2,46 @@
 
 uint32_t base40_encode(char* data)
 {
-	uint32_t x;
-	char* c;
-	
-	/* point c at the end of the data, maximum of 6 characters */
-	for(x = 0, c = data; x < BASE40_FIELD_MAX_CHARS && *c; x++, c++);
-	
-	/* encode it backwards */
-	x = 0;
-	for(c--; c >= data; c--)
-	{
-		x *= 40;
-		if(*c >= 'A' && *c <= 'Z') x += *c - 'A' + 14;
-		else if(*c >= 'a' && *c <= 'z') x += *c - 'a' + 14;
-		else if(*c >= '0' && *c <= '9') x += *c - '0' + 1;
-	}
-	
-	return(x);
+    uint32_t x;
+    char* c;
+
+    /* point c at the end of the data, maximum of 6 characters */
+    for(x = 0, c = data; x < BASE40_FIELD_MAX_CHARS && *c; x++, c++);
+
+    /* encode it backwards */
+    x = 0;
+    for(c--; c >= data; c--)
+    {
+        x *= 40;
+        if(*c >= 'A' && *c <= 'Z') x += *c - 'A' + 14;
+        else if(*c >= 'a' && *c <= 'z') x += *c - 'a' + 14;
+        else if(*c >= '0' && *c <= '9') x += *c - '0' + 1;
+    }
+
+    return(x);
 }
 
 char* base40_decode(char* data, uint32_t code)
 {
-	char* c;
+    char* c;
     char s;
-	
-	*data = '\0';
-	
-	/* is mission_id valid? */
-	if(code > 0xF423FFFF) return(data);
-	
-	for(c = data; code; c++)
-	{
-		s = code % 40;
-		if(s == 0) *c = '-';
-		else if(s < 11) *c = '0' + s - 1;
-		else if(s < 14) *c = '-';
-		else *c = 'A' + s - 14;
-		code /= 40;
-	}
-	*c = '\0';
-	
-	return(data);
+
+    *data = '\0';
+
+    /* is mission_id valid? */
+    if(code > 0xF423FFFF) return(data);
+
+    for(c = data; code; c++)
+    {
+        s = code % 40;
+        if(s == 0) *c = '-';
+        else if(s < 11) *c = '0' + s - 1;
+        else if(s < 14) *c = '-';
+        else *c = 'A' + s - 14;
+        code /= 40;
+    }
+    *c = '\0';
+
+    return(data);
 }
 
--- src/crc32.c
+++ src/crc32.c
@@ -1,21 +1,19 @@
 #include "crc32.h"
 
-
 // standard crc32b algorithm
 uint32_t crc32(const uint8_t *data, uint32_t len) {
-	uint32_t crc = 0xFFFFFFFF;
-	
-	for(uint32_t i = 0; i < len; i++) {
-		uint8_t ch = data[i];
-		for(uint32_t j = 0; j < 8; j++) {
-			uint32_t b = (ch ^ crc) & 1;
-			crc >>= 1;
-			if(b) crc = crc ^ 0xEDB88320;
-			ch >>= 1;
-		}
-	}
-	
-	return ~crc;
-}
+    uint32_t crc = 0xFFFFFFFF;
 
+    for(uint32_t i = 0; i < len; i++) {
+        uint8_t ch = data[i];
+        for(uint32_t j = 0; j < 8; j++) {
+            uint32_t b = (ch ^ crc) & 1;
+            crc >>= 1;
+            if(b) crc = crc ^ 0xEDB88320;
+            ch >>= 1;
+        }
+    }
+
+    return ~crc;
+}
 
