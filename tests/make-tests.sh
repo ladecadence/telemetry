81178b9cf39b69f37ef864c80a37c8b331c7ef63
David Pello <zako@ladecadence.net>
2022-01-18 19:32:51 +0100

    Added debugging symbols for tests

--- tests/make-tests.sh
+++ tests/make-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-gcc -o test-base40 test-base40.c ../src/base40.c
-gcc -o test-crc32 test-crc32.c ../src/crc32.c
-gcc -o test-telemetry test-telemetry.c ../src/telemetry.c ../src/crc32.c ../src/base40.c
-gcc -o test-rawdata test-rawdata.c ../src/telemetry.c ../src/crc32.c ../src/base40.c
-gcc -o test-all test-all.c ../src/telemetry.c ../src/crc32.c ../src/base40.c
+gcc -g -o test-base40 test-base40.c ../src/base40.c
+gcc -g -o test-crc32 test-crc32.c ../src/crc32.c
+gcc -g -o test-telemetry test-telemetry.c ../src/telemetry.c ../src/crc32.c ../src/base40.c
+gcc -g -o test-rawdata test-rawdata.c ../src/telemetry.c ../src/crc32.c ../src/base40.c
+gcc -g -o test-all test-all.c ../src/telemetry.c ../src/crc32.c ../src/base40.c
